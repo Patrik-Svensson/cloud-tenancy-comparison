9bf2645ee8a6b24666d27e8c35999128e73b54f7
Patrik Svensson <extpas@crm.se>
2018-04-12 13:37:19 +0200

    Multitenant solution added

---
+++ ExempelApp/MultiTenant/MigrateDatabase/Functions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace MigrateDatabase
+{
+	public class Functions
+	{
+		// This function will get triggered/executed when a new message is written 
+		// on an Azure Queue called queue.
+		public static void Migrate(TextWriter log)
+		{
+            var configuration = new DbMigrationsConfiguration<WebApplication.DAL.SchoolContext>();
+            var migrator = new DbMigrator(configuration);
+
+            migrator.Update();
+		}
+	}
+}
